1a92b7f650ed8365caa68dc407ea983f861fc4df
soma <marko.sevo7@gmail.com>
2021-05-02 18:12:06 -0600

    Api endpoint for incrementEpoch.

--- main/contracts/ctStake.js
+++ main/contracts/ctStake.js
@@ -7,18 +7,18 @@ const prisma = new PrismaClient()
 
 const { authMiddleware } = require('../utils/auth');
 
-router.post('/api/txStakeConfiguration/send', authMiddleware, async (req, res) => {
+router.post('/api/txStakeDelegation/send', authMiddleware, async (req, res) => {
   try {
     const {
-      configurations,
+      delegations,
     } = req.body;
 
-    console.log(configurations);
+    console.log(delegations);
 
-    const stakeConfigurations = await prisma.txStakeConfiguration.createMany({
-      data: configurations,
+    const stakeDelegations = await prisma.txStakeDelegation.createMany({
+      data: delegations,
     });
-    console.log(stakeConfigurations);
+    console.log(stakeDelegations);
 
     res.send({ result: { success: true, error: false } });
   } catch (err) {
@@ -31,7 +31,7 @@ router.post('/api/txStakeConfiguration/send', authMiddleware, async (req, res) =
   }
 });
 
-router.get('/api/txStakeConfiguration', authMiddleware, async (req, res) => {
+router.get('/api/txStakeDelegation', authMiddleware, async (req, res) => {
   try {
     const {
       fromEthAddress,
@@ -40,7 +40,7 @@ router.get('/api/txStakeConfiguration', authMiddleware, async (req, res) => {
       page,
     } = req.body;
 
-    const stakeConfigurations = await prisma.txStakeConfiguration.findMany({
+    const stakeDelegations = await prisma.txStakeDelegation.findMany({
       where: {
         fromEthAddress,
         toEthAddress,
@@ -49,11 +49,11 @@ router.get('/api/txStakeConfiguration', authMiddleware, async (req, res) => {
       skip: page * PAGINATION_LIMIT,
       take: PAGINATION_LIMIT,
     });
-    console.log(stakeConfigurations);
+    console.log(stakeDelegations);
 
     res.send({
       result: {
-        stakeConfigurations,
+        stakeDelegations,
         error: false,
       },
     });
